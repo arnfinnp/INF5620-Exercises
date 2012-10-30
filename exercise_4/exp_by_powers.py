3c93a7a3d3c3880e83357e8d62217a3032ec8b6b
arnfinnp <arnfinnmp@outlook.com>
2012-10-30 18:52:44 +0100

    exercise 4 done

---
+++ exercise_4/exp_by_powers.py
@@ -0,0 +1,41 @@
+import sympy as sm
+from scitools.std import plot, hold, legend, savefig, linspace, \
+     title, xlabel, axis
+
+def least_squares(f, phi, Omega):
+    N = len(phi) - 1
+    A = sm.zeros((N+1, N+1))
+    b = sm.zeros((N+1, 1))
+    x = sm.Symbol('x')
+    for i in range(N+1):
+        for j in range(i, N+1):
+            A[i,j] = sm.integrate(phi[i]*phi[j],
+                                  (x, Omega[0], Omega[1]))
+            A[j,i] = A[i,j]
+        b[i,0] = sm.integrate(phi[i]*f, (x, Omega[0], Omega[1]))
+    c = A.LUsolve(b)
+    u = 0
+    for i in range(len(phi)):
+        u += c[i,0]*phi[i]
+    return u
+
+def comparison_plot(f, u, Omega, filename='tmp.pdf'):
+    x = sm.Symbol('x')
+    f = sm.lambdify([x], f, modules="numpy")
+    u = sm.lambdify([x], u, modules="numpy")
+    resolution = 401  # no of points in plot
+    xcoor  = linspace(Omega[0], Omega[1], resolution)
+    exact  = f(xcoor)
+    approx = u(xcoor)
+    plot(xcoor, approx)
+    hold('on')
+    plot(xcoor, exact)
+    legend(['approximation', 'exact'])
+    savefig(filename)
+
+x = sm.Symbol('x')
+f = sm.exp(x)
+phi = [1, x, x**2, x**3, x**4, x**5, x**6, x**7]
+Omega = [1, 10]
+u = least_squares(f, phi, Omega)
+comparison_plot(f, u, Omega, 'exp_by_powers.png')
