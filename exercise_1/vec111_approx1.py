3bc6925f25365166f3e2cf29425ef8aa70c75cc0
arnfinnp <arnfinnmp@outlook.com>
2012-10-30 14:49:07 +0100

    exercise 2 done

--- exercise_1/vec111_approx1.py
+++ exercise_1/vec111_approx1.py
@@ -1,3 +1,5 @@
+Exercise 1
+
 Given the vectors u = (x1*a,y1*b), v = (x2*a,y2*b), w = (x3*a,y3*b), 0 = (0,0)
 
 1: u + v 		= (x1*a,y1*b) + (x2*a,y2*b)
@@ -51,3 +53,4 @@ Given the vectors u = (x1*a,y1*b), v = (x2*a,y2*b), w = (x3*a,y3*b), 0 = (0,0)
 				= (1*x1*a,1*y1*b)
 				= (x1*a,y1*b)
 				= u
+
