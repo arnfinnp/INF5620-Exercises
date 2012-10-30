aedc1fc47ffa100e589d0b709917eeb8e8db9286
arnfinnp <arnfinnmp@outlook.com>
2012-10-30 14:20:42 +0100

    first part of ex1 done

---
+++ exercise_1/vec111_approx1.py
@@ -0,0 +1,53 @@
+Given the vectors u = (x1*a,y1*b), v = (x2*a,y2*b), w = (x3*a,y3*b), 0 = (0,0)
+
+1: u + v 		= (x1*a,y1*b) + (x2*a,y2*b)
+				= (x1*a + x2*a,y1*b + y2*b) 
+				= ([x1+x2]a,[y1+y2]b) 
+				= (x*a,y*b)
+
+2: u + v 		= (x1*a,y1*b) + (x2*a,y2*b)
+				= (x1*a + x2*a,y1*b + y2*b) 
+				= ([x1+x2]a,[y1+y2]b) 
+				= ([x2+x1]a,[y2+y1]b)
+				= (x2*a + x1*a,y2*b + y1*b)
+				= (x2*a,y2*b) + (x1*a,y1*b)
+				= v + u
+
+3: (u + v) + w 	= ( (x1*a,y1*b) + (x2*a,y2*b) ) + (x3*a,y3*b)
+				= ([x1+x2]a,[y1+y2]b) + (x3*a,y3*b)
+				= ([x1+x2+x3]a,[y1+y2+y3]b)
+				= (x1*a + [x2+x3]a,y1*b + [y2+y3]b)
+				= (x1*a,y1*b) + ([x2+x3]a, [y2+y3]b)
+				= u + ( (x2*a,y2*b) + (x3*a,y3*b) )
+				= u + (v + w)
+
+4: u + 0 		= (x1*a,y1*b) + (0,0)
+				= (x1*a + 0,y1*b + 0)
+				= (x1*a,y1*b) 
+				= u
+
+5: u + (-u) 	= (x1*a,y1*b) + (-(x1*a,y1*b))
+				= (x1*a,y1*b) + (-x1*a,-y1*b))
+				= ([x1-x1]a,[y1-y1]*b)
+				= (0*a,0*b)
+				= 0
+
+7: c(u + v)		= c*( (x1*a,y1*b) + (x2*a,y2*b) )
+				= c*(x1*a,y1*b) + c*(x2*a,y2*b)
+				= cu +cv
+
+8: (c + d)u		= (c + d)*(x1*a,y1*b)
+				= c*(x1*a,y1*b) + d*(x1*a,y1*b)
+				= cu + du
+
+9: c(du) 		= c*(d*(x1*a,y1*b))
+				= c*(d*x1*a,d*y1*b)
+				= (c*d*x1*a,c*d*y1*b)
+				= ((c*d)*x1*a,(c*d)*y1*b)
+				= (c*d)*(x1*a,y1*b)	 
+				= (cd)u
+
+10: 1u 			= 1*(x1*a,y1*b)
+				= (1*x1*a,1*y1*b)
+				= (x1*a,y1*b)
+				= u
